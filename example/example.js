69cd3affccd877cd7009cb70194893227e8a6ee4
Maximilian <maximilian.greschke@gmail.com>
2015-05-22 21:01:33 +0200

    removed IDE folder and slight change on invoice.complete

---
+++ example/example.js
@@ -0,0 +1,39 @@
+/*
+ * fastbill automatic api wrapper for nodejs
+ *
+ * Copyright(c) 2015 maximilian greschke <maximilian.greschke@gmail.com>
+ * MIT Licensed
+ *
+ * This example demonstrates how to use node-fastbill-automatic.
+ *
+ */
+
+/**
+ * @author Maximilian Greschke <maximilian.greschke@gmail.com>
+ *
+ */
+
+// use node in --harmony mode to run this example
+'use strict';
+
+var
+    config = require('./config'),
+    fastbill = require('../index');
+
+// 1. create a customer
+
+fastbill.customer.create(
+    {
+        CUSTOMER_NUMBER: '1',
+        CUSTOMER_TYPE: 'consumer',
+        FIRST_NAME: 'Max',
+        LAST_NAME: 'Michael'
+    }, function(err, data){
+        if (err) throw new Error('Customer creation failed');
+
+        // 2. create an invoice
+       //TODO: continue example
+    });
+
+
+
